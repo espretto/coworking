1b9f71626bf9bb1ea1d30c9e65e0cf2c14373b04
espretto <espretto@posteo.net>
2020-02-26 16:57:53 +0100

    make axios deserialize dates from json

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,13 @@
 
-import L from 'leaflet'
-
+/**
+ * coworking api config
+ */
 export const apiVersion = '0.1.0'
+
+/**
+ * leaflet config
+ */
+import L from 'leaflet'
 
 export const leafletOptions = {
   zoom: 12,
@@ -13,3 +19,24 @@ export const leafletOptions = {
 
 // adjust image path to environment
 L.Icon.Default.prototype.options.imagePath = leafletOptions.imageBasePath
+L.Icon.Default.prototype.options.iconRetinaUrl = L.Icon.Default.prototype.options.iconUrl
+
+/**
+ * axios (ajax) config
+ */
+import axios from 'axios'
+
+var ISO_8601 = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
+
+function dateReviver (key, value) {
+  return typeof value === 'string' && ISO_8601.test(value) ? new Date(value) : value
+}
+
+axios.defaults.transformResponse = [function (data) {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data, dateReviver);
+    } catch (e) { /* Ignore */ }
+  }
+  return data;
+}]
